85a5902ecde10d6e51182b1bf0b2e7f7f0214430
Gabriel Gómez <gabomgp4@gmail.com>
2020-07-20 16:29:47 -0500

    POST is working

--- src/main/kotlin/com/virtualpairprogrammers/theater/control/MainController.kt
+++ src/main/kotlin/com/virtualpairprogrammers/theater/control/MainController.kt
@@ -1,12 +1,38 @@
 package com.virtualpairprogrammers.theater.control
 
+import com.virtualpairprogrammers.theater.services.BookingService
+import com.virtualpairprogrammers.theater.services.TheaterService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
-class MainController {
+class MainController(
+        val theaterService: TheaterService,
+        val bookingService: BookingService
+) {
+    @RequestMapping("")
+    fun homePage() =
+            ModelAndView("seatBooking", "bean", CheckAvailabilityBackingBean())
 
-    @RequestMapping("helloWorld")
-    fun helloWorld() = run { ModelAndView("helloWorld") }
+    @RequestMapping("checkAvailability", method = [RequestMethod.POST])
+    fun checkAvailability(bean: CheckAvailabilityBackingBean) = run {
+        val selectedSeat = theaterService.find(bean.selectedSeatRow, bean.selectedSeatNum)
+        val result = bookingService.isSeatFree(selectedSeat)
+        ModelAndView("seatBooking", mapOf("bean" to bean.copy(
+                result = "Seat $selectedSeat is ${if (result) "available" else "booked"}"
+        )))
+    }
+
+}
+
+data class CheckAvailabilityBackingBean(
+        val selectedSeatNum: Int = 1,
+        val selectedSeatRow: Char = 'A',
+        val result: String = ""
+) {
+    val seatNums = 1..36
+    val seatRows = 'A'..'O'
 }
